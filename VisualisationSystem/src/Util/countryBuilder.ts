54db9cdf580bb46680160e7c2529e0703f50533a
AdenKenny <adenkenny@gmail.com>
2018-11-02 13:16:38 +1300

    fix

--- VisualisationSystem/src/Util/countryBuilder.ts
+++ VisualisationSystem/src/Util/countryBuilder.ts
@@ -1,4 +1,5 @@
-; "use strict";
+; 
+"use strict";
 
 import Country from "./country";
 
@@ -9,16 +10,16 @@ class CountryBuilder {
     private buildName: string | null;
     private buildAlternativeName: string;
     private buildRegion: string;
-    private buildBudget: { expenditure: string, revenue: string };
+    private buildBudget: { expenditure: number, revenue: number };
     private buildGdpBySector: { agriculture: string, industry: string, services: string };
-    private buildGini: { years: Map<string, string>, rank: string };
-    private buildGrowthRate: { years: Map<string, string>, rank: string };
-    private buildHouseholdIncome: { top: string, bottom: string };
+    private buildGini: { years: Map<string, string>, rank: number };
+    private buildGrowthRate: { years: Map<string, number>, rank: number };
+    private buildHouseholdIncome: { highest_10: number, lowest_10: number };
     private buildInflation: { years: Map<string, string>, rank: string };
-    private buildLabourForce: { rank: string, size: string, occupations: Map<string, string> };
-    private buildPopulation: { total: string, rank: string };
-    private buildPopulationPovertyLine: string;
-    private buildPpp: { years: Map<string, string>, rank: string };
+    private buildLabourForce: { global_rank: number, total_people: number, by_occupation: Map<string, number> };
+    private buildPopulation: { total: number, global_rank: number };
+    private buildPopulationPovertyLine: number;
+    private buildPpp: { years: Map<string, number>, rank: number };
     private buildPublicDebt: { years: Map<string, string>, rank: string };
     private buildSavings: { years: Map<string, string>, rank: string };
     private buildUnemployment: { years: Map<string, string>, rank: string };
@@ -27,18 +28,18 @@ class CountryBuilder {
     private alternativeName: string;
     private region: string;
     private budget: { expenditure: number, revenue: number };
-    private gdpBySector: { industries: Map<string, number>, rank: number };
-    private gini: { years: Map<string, number>, rank: number };
-    private growthRate: { years: Map<string, number>, rank: number };
+    private gdpBySector: { industries: Map<string, number>};
+    private gini: { years: Map<number, number>, rank: number };
+    private growthRate: { years: Map<number, number>, rank: number };
     private householdIncome: { top: number, bottom: number };
-    private inflation: { years: Map<string, number>, rank: string };
+    private inflation: { years: Map<number, number>, rank: number };
     private labourForce: { rank: number, size: number, occupations: Map<string, number> };
     private population: { total: number, rank: number };
-    private populationBelowPovertyLine: string;
-    private ppp: { years: Map<string, number>, rank: number };
-    private publicDebt: { years: Map<string, number>, rank: number };
-    private savings: { years: Map<string, number>, rank: number };
-    private unemployment: { years: Map<string, number>, rank: number };
+    private populationBelowPovertyLine: number;
+    private ppp: { years: Map<number, number>, rank: number };
+    private publicDebt: { years: Map<number, number>, rank: number };
+    private savings: { years: Map<number, number>, rank: number };
+    private unemployment: { years: Map<number, number>, rank: number };
 
     constructor(rawName: string | null, rawData: any) {
 
@@ -65,26 +66,146 @@ class CountryBuilder {
     build(): Country {
 
         this.name = this.cleanupName(this.buildName);
-        this.alternativeName = this.cleanupName(this.buildName);
+        this.alternativeName = this.cleanupName(this.buildAlternativeName);
         this.region = this.cleanupName(this.buildRegion);
 
-        try {
-            this.budget = {
-                expenditure: this.strToNumber(this.buildBudget.expenditure),
-                revenue: this.strToNumber(this.buildBudget.revenue)
+        this.mTry(() => {
+            if (this.buildBudget !== undefined) {
+                this.budget = {
+                    expenditure: this.buildBudget.expenditure,
+                    revenue: this.buildBudget.revenue
+                };    
+            }
+        });
+
+        this.mTry(() => {
+            this.population = {
+                total: this.buildPopulation.total,
+                rank: this.buildPopulation.global_rank
             };
-        }
+        });
 
-        catch (error) {
+        this.mTry(() => {
+            this.populationBelowPovertyLine = this.buildPopulationPovertyLine;
+        });
 
-        }
+        this.mTry(() => {
+            if (this.buildGini !== undefined) {
+                this.gini = this.getYears(this.buildGini);
+            }
+        });
 
+        this.mTry(() => {
+            if (this.buildPpp !== undefined) {
+                this.ppp = this.getYears(this.buildPpp);
+            }
+        });
 
-        console.log(this);
+        this.mTry(() => {
+            if (this.buildSavings !== undefined) {
+                this.savings = this.getYears(this.buildSavings);
+            }
+        });
+
+        this.mTry(() => {
+            if (this.buildUnemployment !== undefined) {
+                this.unemployment = this.getYears(this.buildUnemployment);
+            }
+        });
+
+        this.mTry(() => {
+            if (this.buildPublicDebt !== undefined) {
+                this.publicDebt = this.getYears(this.buildPublicDebt);
+            }
+        });
+
+        this.mTry(() => {
+            if (this.buildInflation !== undefined) {
+                this.inflation = this.getYears(this.buildInflation);
+            }
+        });
+        
+        this.mTry(() => {
+            if (this.buildGrowthRate !== undefined) {
+                this.growthRate = this.getYears(this.buildGrowthRate);
+            }
+        });
+
+        this.mTry(() => {
+            if (this.buildHouseholdIncome !== undefined) {
+                this.householdIncome = {
+                    top: this.buildHouseholdIncome.highest_10,
+                    bottom: this.buildHouseholdIncome.lowest_10
+                };
+            }
+        });
+
+        this.mTry(() => {
+            if (this.buildGdpBySector !== undefined) {
+                
+                const map = new Map<string, number>();
+
+                map["agriculture"] = this.buildGdpBySector.agriculture;
+                map["industry"] = this.buildGdpBySector.industry;
+                map["services"] = this.buildGdpBySector.services;
+
+                this.gdpBySector = {
+                    industries: map
+                };
+            }
+        });
+
+        this.mTry(() => {
+            if (this.buildLabourForce !== undefined) {
+
+                const map = new Map<string, number>();
+
+                map["agriculture"] = this.buildLabourForce.by_occupation["agriculture"];
+                map["industry"] = this.buildLabourForce.by_occupation["industry"];
+                map["services"] = this.buildLabourForce.by_occupation["services"];
+
+                this.labourForce = {
+                    rank: this.buildLabourForce.global_rank,
+                    size: this.buildLabourForce.total_people,
+                    occupations: map
+                };
+            }
+        });
         return new Country(this);
     }
 
-    strToNumber = (str: string): number => {
+    private getYears = (proprety: any): {rank: number, years: Map<number, number>} => {
+        const obj = {
+            rank:  proprety.rank,
+            years: new Map()
+        };
+
+        const keys = Object.keys(proprety);
+
+        const mapOfVals = new Map<number, number>();
+
+        keys.forEach(k => {
+            if (k !== "rank") {
+                mapOfVals[k] = proprety[k];
+            }
+        });
+
+        obj.years = mapOfVals;
+
+        return obj;
+    }
+
+    private mTry = (func: () => void): void => {
+        try {
+            return func();
+        }
+
+        catch (error) {
+            //console.log(error);
+        }
+    }
+
+    private strToNumber = (str: string): number => {
 
         if (str !== undefined) {
             if (str.indexOf('.') > -1) { // We need to parse this to a float.
@@ -100,70 +221,6 @@ class CountryBuilder {
         }
 
         return NaN;
-    }
-
-    public get $name(): string {
-        return this.name;
-    }
-
-    public get $alternativeName(): string {
-        return this.alternativeName;
-    }
-
-    public get $region(): string {
-        return this.region;
-    }
-
-    public get $budget(): { expenditure: number, revenue: number } {
-        return this.budget;
-    }
-
-    public get $gdpBySector(): { industries: Map<string, number>, rank: number } {
-        return this.gdpBySector;
-    }
-
-    public get $gini(): { years: Map<string, number>, rank: number } {
-        return this.gini;
-    }
-
-    public get $growthRate(): { years: Map<string, number>, rank: number } {
-        return this.growthRate;
-    }
-
-    public get $householdIncome(): { top: number, bottom: number } {
-        return this.householdIncome;
-    }
-
-    public get $inflation(): { years: Map<string, number>, rank: string } {
-        return this.inflation;
-    }
-
-    public get $labourForce(): { rank: number, size: number, occupations: Map<string, number> } {
-        return this.labourForce;
-    }
-
-    public get $population(): { total: number, rank: number } {
-        return this.population;
-    }
-
-    public get $populationBelowPovertyLine(): string {
-        return this.populationBelowPovertyLine;
-    }
-
-    public get $ppp(): { years: Map<string, number>, rank: number } {
-        return this.ppp;
-    }
-
-    public get $publicDebt(): { years: Map<string, number>, rank: number } {
-        return this.publicDebt;
-    }
-
-    public get $savings(): { years: Map<string, number>, rank: number } {
-        return this.savings;
-    }
-
-    public get $unemployment(): { years: Map<string, number>, rank: number } {
-        return this.unemployment;
     }
 
     private cleanupName = (rawName: string | null): string => {
@@ -186,6 +243,71 @@ class CountryBuilder {
         }
 
         return name.trim();
+    }
+
+
+    public get $name(): string {
+        return this.name;
+    }
+
+    public get $alternativeName(): string {
+        return this.alternativeName;
+    }
+
+    public get $region(): string {
+        return this.region;
+    }
+
+    public get $budget(): { expenditure: number, revenue: number } {
+        return this.budget;
+    }
+
+    public get $gdpBySector(): { industries: Map<string, number>} {
+        return this.gdpBySector;
+    }
+
+    public get $gini(): { years: Map<number, number>, rank: number } {
+        return this.gini;
+    }
+
+    public get $growthRate(): { years: Map<number, number>, rank: number } {
+        return this.growthRate;
+    }
+
+    public get $householdIncome(): { top: number, bottom: number } {
+        return this.householdIncome;
+    }
+
+    public get $inflation(): { years: Map<number, number>, rank: number } {
+        return this.inflation;
+    }
+
+    public get $labourForce(): { rank: number, size: number, occupations: Map<string, number> } {
+        return this.labourForce;
+    }
+
+    public get $population(): { total: number, rank: number } {
+        return this.population;
+    }
+
+    public get $populationBelowPovertyLine(): number {
+        return this.populationBelowPovertyLine;
+    }
+
+    public get $ppp(): { years: Map<number, number>, rank: number } {
+        return this.ppp;
+    }
+
+    public get $publicDebt(): { years: Map<number, number>, rank: number } {
+        return this.publicDebt;
+    }
+
+    public get $savings(): { years: Map<number, number>, rank: number } {
+        return this.savings;
+    }
+
+    public get $unemployment(): { years: Map<number, number>, rank: number } {
+        return this.unemployment;
     }
 }
 
