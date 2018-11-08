936d614cdbd61c891059a1e4932424b98d0f3b7d
zhengzhon <zhengzhon@ecs.vuw.ac.nz>
2018-11-08 17:18:00 +1300

    preparing for merge

--- VisualisationSystem/src/components/Bars/Bars.tsx
+++ VisualisationSystem/src/components/Bars/Bars.tsx
@@ -1,12 +1,11 @@
 import * as d3 from 'd3';
 import React, { Component } from 'react';
-import NodeGroup from 'react-move/NodeGroup';
+import Animate from "react-move/Animate";
 import { scaleLinear } from 'd3-scale';
 import { interpolateLab } from 'd3-interpolate';
 
 import './Bars.css';
 import Tooltip from 'rc-tooltip';
-import Animate from 'rc-animate';
 import 'rc-tooltip/assets/bootstrap.css';
 
 export default class Bars extends Component<{maxValue, scales, margins, data, svgDimensions}> {
@@ -23,21 +22,21 @@ export default class Bars extends Component<{maxValue, scales, margins, data, sv
     const bars = [];
     data.forEach((datum, i) => {
       bars.push(
+        
        <Tooltip 
        placement='rightTop' 
-       overlay={data[i].name + ': ' + Bars.commafier(data[i].value)}
+       overlay={datum.name + ': ' + Bars.numberFormatter(datum.value)}
        mouseLeaveDelay={0}
        >
-        <rect
-          id={i}
-          key={i}
-          className='bars' 
-          x={xScale(datum.name)}
-          y={yScale(datum.value)}
-          height={height - margins.bottom - scales.yScale(datum.value)}
-          width={xScale.bandwidth()}
-          // start={(data,index)}
-          />      
+          <rect
+            id={i}
+            key={i}
+            className='bars' 
+            x={xScale(datum.name)}
+            y={yScale(datum.value)}
+            height={height - margins.bottom - scales.yScale(datum.value)}
+            width={xScale.bandwidth()}
+            />
         </Tooltip>    
           
       );
@@ -50,11 +49,18 @@ export default class Bars extends Component<{maxValue, scales, margins, data, sv
     );
   }
 
-  static commafier = (input:number) => {
-    if(input !== undefined)
-      return input.toLocaleString(navigator.language);
-    else
+  static numberFormatter = (input: number) => {
+    if(input === undefined){
       return 0;
+    } else if (input > 999999999) {
+      return (input / 1000000000).toLocaleString(navigator.language) + 'b';
+    } else if (input > 999999) {
+      return (input / 1000000).toLocaleString(navigator.language) + 'm';
+    } else if (input > 999) {
+      return (input / 1000).toLocaleString(navigator.language) + 'k';
+    } else {
+      return input;
+    }
   }
 
 }
