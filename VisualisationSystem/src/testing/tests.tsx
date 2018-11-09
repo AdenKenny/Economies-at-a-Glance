c246d2d2a6727c9d536825eac5b56020e6d5eebc
Ryan <ratchetmid@yahoo.co.nz>
2018-11-09 22:27:38 +1300

    unit tests, one for each dataHandler parameter

--- VisualisationSystem/src/testing/tests.tsx
+++ VisualisationSystem/src/testing/tests.tsx
@@ -10,11 +10,11 @@ describe('Tests', () => {
         expect(true).to.equal(true);
     });
 
-    it('data should be empty as no data is loaded', () => {
+    it('data should be empty as no countries are loaded', () => {
         expect(countryData).is.undefined;
     });
 
-    it('data should not be empty as data is fully loaded', (done) => {
+    it('data should not be empty as countries are fully loaded', (done) => {
         db.readFromDb().then(country => {
             countryData = country;
 
@@ -42,13 +42,42 @@ describe('Tests', () => {
     });
 
     it('country New Zealand should be in continent Oceania', () => {
-        //console.log(countryData.get('new_zealand'));
         expect(countryData.get('new_zealand').region).to.equal('Oceania');
 
     });
 
     it('country New Zealand should have a total revenue of 73,200,000,000', () => {
         expect(countryData.get('new_zealand').budget['revenue']).to.equal(73200000000);
+
+    });
+
+    it('country New Zealand should have a Purchasing Power Parity of 188,600,000,000 in 2017', () => {
+        expect(countryData.get('new_zealand').ppp['years'].get(2017)).to.equal(188600000000);
+
+    });
+
+    it('country New Zealand should have an Unemployment Rate of 4.9% in 2017', () => {
+        expect(countryData.get('new_zealand').unemployment['years'].get(2017)).to.equal(4.9);
+
+    });
+
+    it('country New Zealand should have an Global Unemployment Rank of 72', () => {
+        expect(countryData.get('new_zealand').unemployment['rank']).to.equal(72);
+
+    });
+
+    it('country New Zealand should have an Global Inflation Rank of 96', () => {
+        expect(countryData.get('new_zealand').inflation['rank']).to.equal(96);
+
+    });
+
+    it('country New Zealand should have an Inflation Rate of 1.9 in 2017', () => {
+        expect(countryData.get('new_zealand').inflation['years'].get(2017)).to.equal(1.9);
+
+    });
+
+    it('country New Zealand should have a population greater than four million', () => {
+        expect(countryData.get('new_zealand').population['total']).greaterThan(4000000);
 
     });
 
